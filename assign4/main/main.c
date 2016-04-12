a6555376ca0ddd7e1253c8ddc1bff119c0f7e00d
andyafter <andyafter@gmail.com>
2016-04-12 13:48:06 +0800

    Added Makefile

--- assign4/main/main.c
+++ assign4/main/main.c
@@ -2,26 +2,28 @@
 #include<time.h>
 
 
-#define X 10000
-#define Y 10000
+#define X 1000
+#define Y 1000
+#define Z 1000
+
 
 int main(){
-  int a[10000][10000] = {0};
-  int b[10000][10000] = {0};
-  int c[10000][10000] = {0};
+  int a[X][Y] = {0};
+  int b[Y][Z] = {0};
+  int c[X][Z] = {0};
   int i, j, k;
   clock_t begin, end;
   double time_spent;
   begin = clock();
-  for(i=0;i<10000;++i){
-    for(j=0;j<10000;++j){
-      for(k=0;k<10000;++k){
+  for(i=0;i<X;++i){
+    for(j=0;j<Y;++j){
+      for(k=0;k<Z;++k){
         c[i][k]+= a[i][j]*b[j][k];
       }
     }
   }
   end = clock();
   time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-
+  printf("time spent: %f \n", time_spent);
   return 0;
 }
