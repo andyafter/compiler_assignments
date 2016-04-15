dbca59e9f3ade93ceb8568857bf0f77bafa540af
andyafter <andyafter@gmail.com>
2016-04-15 16:51:30 +0800

    Added basic loop tiling.

--- assign4/main/main.c
+++ assign4/main/main.c
@@ -6,6 +6,7 @@
 #define X 20000
 #define Y 30000
 #define Z 40000
+#define block_size 100
 
 void print_matrix(float *mat, int i, int j);
 void matrix_multiply(float *mat1, float *mat2, float *result, int i, int j, int k);
@@ -78,10 +79,34 @@ void matrix_multiply(float *mat1, float *mat2, float *result, int i, int j, int 
 
 }
 
+
 void tiling_mm(float *mat1, float *mat2, float *result, int i, int j, int k){
     // this function take cares of basic loop tiling
-    int m, n, l;
+    int m, n, mm, l; // mm however, is the block index, nothing to do with m.
     double time_spent;
     clock_t begin, end;
+    for(m=0;m<i;m+=block_size){
+        begin = clock();
+        for(l=0; l<k; l+=block_size){
+            #pragma omp parallel for
+            for(n=0; n<j; ++n){
+                // result does not have anything to do with n,
+                // which n is the column of mat1 and row of mat2
+                // here block stuff.
+                for(mm=0; mm<block_size; ++mm){
+                    result[m*k+mm*k+n]+=\
+                        mat1[m*j+mm*j+n]*mat2[n*k+l+mm];
+                }
+            }
+        }
+        end = clock();
+        time_spent = (double)(end - begin) / CLOCKS_PER_SEC;            
+        printf("$d\t%d\tTime Spent:  %f\n", m,i, time_spent );
+    }
+}
+
+
+void sync_mm(float *mat1, float *mat2, float *result, int i, int j, int k){
+    // synchronous multiplication
     
 }
